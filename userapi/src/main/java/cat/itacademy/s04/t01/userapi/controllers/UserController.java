9a435fac6dc8599410cbebe3fc71764b8e8071e2
JordiCasas87 <Jordicasas87@gmail.com>
2025-12-08 12:43:14 +0100

    feat: added class UserController and endpoint/users

--- userapi/src/main/java/cat/itacademy/s04/t01/userapi/controllers/UserController.java
+++ userapi/src/main/java/cat/itacademy/s04/t01/userapi/controllers/UserController.java
@@ -4,7 +4,6 @@ import cat.itacademy.s04.t01.userapi.models.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 import java.util.ArrayList;
 
 @RestController
