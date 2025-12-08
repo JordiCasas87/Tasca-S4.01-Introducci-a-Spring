17cb36a557aac3e25dc9cd787766bf1307471f73
JordiCasas87 <Jordicasas87@gmail.com>
2025-12-08 13:32:33 +0100

    feat: level2 create new Postmapping whith new method for create newUser using PostMan

--- userapi/src/main/java/cat/itacademy/s04/t01/userapi/controllers/UserController.java
+++ userapi/src/main/java/cat/itacademy/s04/t01/userapi/controllers/UserController.java
@@ -2,6 +2,8 @@ package cat.itacademy.s04.t01.userapi.controllers;
 
 import cat.itacademy.s04.t01.userapi.models.User;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -14,6 +16,14 @@ public class UserController {
     @GetMapping("/users")
     public ArrayList<User> showUsers (){
         return userList;
+    }
+
+    @PostMapping ("/users")
+    public User createUser (@RequestBody User user){
+        User newUser = new User(user.getName(), user.getEmail());
+        userList.add(newUser);
+        return newUser;
+
     }
 
 
