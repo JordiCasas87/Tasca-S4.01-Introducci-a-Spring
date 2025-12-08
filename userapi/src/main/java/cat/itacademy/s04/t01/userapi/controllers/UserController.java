e98af5ef4fafe3619eb3da114001e4a9cf979db9
JordiCasas87 <Jordicasas87@gmail.com>
2025-12-08 12:38:58 +0100

    feat: level2 create class User

---
+++ userapi/src/main/java/cat/itacademy/s04/t01/userapi/controllers/UserController.java
@@ -0,0 +1,21 @@
+package cat.itacademy.s04.t01.userapi.controllers;
+
+import cat.itacademy.s04.t01.userapi.models.User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import java.util.ArrayList;
+
+@RestController
+public class UserController {
+
+    static ArrayList <User> userList = new ArrayList<>();
+
+    @GetMapping("/users")
+    public ArrayList<User> showUsers (){
+        return userList;
+    }
+
+
+}
---
+++ userapi/src/main/java/cat/itacademy/s04/t01/userapi/models/User.java
@@ -0,0 +1,11 @@
+package cat.itacademy.s04.t01.userapi.models;
+
+import java.util.UUID;
+
+public class User {
+
+    UUID id;
+    String name;
+    String email;
+
+}
