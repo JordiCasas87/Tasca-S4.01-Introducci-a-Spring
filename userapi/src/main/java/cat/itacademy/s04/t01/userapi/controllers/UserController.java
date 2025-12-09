cd23f6fcfab667e802485a26806c9f78d8fb8758
JordiCasas87 <Jordicasas87@gmail.com>
2025-12-09 11:41:36 +0100

    feat: level2, new endpoint para encontrar User por ID, uso de PathVariable

--- userapi/src/main/java/cat/itacademy/s04/t01/userapi/controllers/UserController.java
+++ userapi/src/main/java/cat/itacademy/s04/t01/userapi/controllers/UserController.java
@@ -1,12 +1,11 @@
 package cat.itacademy.s04.t01.userapi.controllers;
 
+import cat.itacademy.s04.t01.userapi.exceptions.NotFoundId;
 import cat.itacademy.s04.t01.userapi.models.User;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.UUID;
 
 @RestController
 public class UserController {
@@ -23,8 +22,20 @@ public class UserController {
         User newUser = new User(user.getName(), user.getEmail());
         userList.add(newUser);
         return newUser;
-
     }
+
+    @GetMapping("/users/{id}")
+    public User getUserId(@PathVariable UUID id) {
+
+        for (User userId : userList) {
+            if (userId.getId().equals(id)) {
+                return userId;
+            }
+        }
+        throw new NotFoundId("Id no encontrado");
+    }
+
+
 
 
 }
